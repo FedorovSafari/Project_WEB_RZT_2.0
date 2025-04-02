9ff0cc6e9426cfea50968eb78a01890ac71f8559
FedorovSafari <zemlopol@gmail.com>
2025-04-02 22:53:19 +0700

    так надо

--- server/index.js
+++ server/index.js
@@ -5,13 +5,10 @@ const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandingMiddleware');
 const app = express();
-
-const { fetchAndSaveArtists } = require('./API/loading_artist');
-const { fetchAndSaveGenres } = require('./API/loading_genre');
-const { fetchAndSaveTracks } = require('./API/loading_track');
-const { fetchAndSaveAlbums } = require('./API/loading_album');
-
-
+//const { fetchAndSaveArtists } = require('./API/loading_artist');
+//const { fetchAndSaveGenres } = require('./API/loading_genre');
+//const { fetchAndSaveTracks } = require('./API/loading_track');
+//const { fetchAndSaveAlbums } = require('./API/loading_album');
 
 app.use(cors());
 app.use(express.json());
@@ -19,8 +16,10 @@ app.use('/api', router);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 6000;
-const artistNames = ['boulevard depo'];
-const tracksLimit = 10;
+
+//const artistNames = ['boulevard depo'];
+//const tracksLimit = 10;
+
 // Запуск приложения
 const start = async () => {
     try {
@@ -32,20 +31,20 @@ const start = async () => {
     );
 
         // Загружаем данные артистов после старта сервера
-        await fetchAndSaveArtists(artistNames);
+   //     await fetchAndSaveArtists(artistNames);
 
         // Загружаем жанры после старта сервера
-        await fetchAndSaveGenres();
+    //    await fetchAndSaveGenres();
 
         // Загружаем треки для каждого артиста
-       // for (const artistName of artistNames) {
-        //    await fetchAndSaveTracks(artistName, tracksLimit);
-      //  }
+    //    for (const artistName of artistNames) {
+    //       await fetchAndSaveTracks(artistName, tracksLimit);
+    //    }
 
         // Загружаем альбомы для каждого артиста
-        for (const artistName of artistNames) {
-            await fetchAndSaveAlbums(artistName);
-        }
+    //    for (const artistName of artistNames) {
+    //        await fetchAndSaveAlbums(artistName);
+    //    }
 
 
     } catch (e) {
